e68daca61b3304ed0de9e767959f19b0a12d9bd7
ranamohyy <ranaamr22@gmail.com>
2025-08-27 12:25:17 +0300

    refactor code

--- lib/features/home_nav/screens/home_page/view.dart
+++ lib/features/home_nav/screens/home_page/view.dart
@@ -5,19 +5,23 @@ import '../../../widgets/categories_item.dart';
 import '../../../widgets/deals_item.dart';
 
 class HomePageScreen extends StatelessWidget {
-   HomePageScreen({super.key});
-  List<String>labels=["Fruits","Vegtables", "Meat","Fish"];
-  List<String>images=["assets/images/enabbb.png","assets/images/vagtables.png","assets/images/vagtables.png","assets/images/vagtables.png"];
-  List<int>colors=[0xffEDD0FF,0xffFFD9BA,0xffFACCCC,0xffFACCCC];
-  List<String>fruitName=["Apple","Banana", "Apple","Banana"];
-  List<String>fruitImage=[    AppImages.appleImage,
-
+  HomePageScreen({super.key});
+  List<String> labels = ["Fruits", "Vegtables", "Meat", "Fish"];
+  List<String> images = [
+    AppImages.enabImage,
     AppImages.vagtablesImage,
     AppImages.appleImage,
-   AppImages.appleImage,
-
-   ];
-  List<String>prices=["900","800","700","500"];
+    AppImages.appleImage,
+  ];
+  List<int> colors = [0xffEDD0FF, 0xffFFD9BA, 0xffFACCCC, 0xffFACCCC];
+  List<String> fruitName = ["Apple", "Banana", "Apple", "Banana"];
+  List<String> fruitImage = [
+    AppImages.appleImage,
+    AppImages.vagtablesImage,
+    AppImages.appleImage,
+    AppImages.appleImage,
+  ];
+  List<String> prices = ["900", "800", "700", "500"];
 
   @override
   Widget build(BuildContext context) {
@@ -29,23 +33,28 @@ class HomePageScreen extends StatelessWidget {
           child: ListView.builder(
             itemCount: labels.length,
             scrollDirection: Axis.horizontal,
-            itemBuilder:(context, index) {
+            itemBuilder: (context, index) {
               return Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: CategoriesItem(label: labels[index],
+                child: CategoriesItem(
+                  label: labels[index],
                   backgroundColor: Color(colors[index]),
-                  image: images[index],),
-              );},),),
+                  image: images[index],
+                ),
+              );
+            },
+          ),
+        ),
         SizedBox(
           height: 250,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
             itemCount: fruitName.length,
-            itemBuilder:(context, index) =>
-                DealsItem(fruitImage: fruitImage[index],price:prices[index],fruitName: fruitName[index],),
-
-
-
+            itemBuilder: (context, index) => DealsItem(
+              fruitImage: fruitImage[index],
+              price: prices[index],
+              fruitName: fruitName[index],
+            ),
           ),
         )
       ],
