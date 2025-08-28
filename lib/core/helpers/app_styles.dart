748c5d7379b17af8be0222f59a38fa50b47affe5
ranamohyy <ranaamr22@gmail.com>
2025-08-28 13:46:01 +0300

    add app styles

---
+++ lib/core/helpers/app_styles.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+class AppTextStyles{
+  static const kHeadText20=TextStyle(
+    color: AppColors.secondColor,
+    fontWeight: FontWeight.normal,
+    fontSize: 20,
+  );
+  static const kTextStyle20bold=TextStyle(
+    color: AppColors.primaryColor,
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+  );
+  static const kHeadText14green=TextStyle(
+    color: AppColors.greenColor,
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+  static const kHeadText14Red=TextStyle(
+    color: Colors.red,
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+  static const kTextStyle18Profile=TextStyle(
+    color: AppColors.secondColor,
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+  );
+  static const kTextAppBarStyle= TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    color:AppColors.primaryColor,
+  );
+  static const kTextStyle16pPrimary=  TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      color: AppColors.primaryColor);
+  static const kTextStyle16SecondPrimary=   TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  color:AppColors.secondColor);
+  static const kTextStyle18secondColor=   TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w700,
+  color:AppColors.secondColor);
+  static const kTextStyleTab=   TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+  color:AppColors.secondColor);
+
+  static const kTextStyle20SecondPrimary=   TextStyle(
+    color:AppColors.secondColor,
+    fontWeight: FontWeight.normal,
+    fontSize: 20,
+  );
+
+
+
+}
