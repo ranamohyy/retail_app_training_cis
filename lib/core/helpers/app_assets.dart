7a529e0cb44608099fa91bd54184197b446d53af
ranamohyy <ranaamr22@gmail.com>
2025-08-28 13:45:09 +0300

    add assets

--- lib/core/helpers/app_assets.dart
+++ lib/core/helpers/app_assets.dart
@@ -2,6 +2,12 @@ class AppImages{
  static const String appleImage="assets/images/apple.png";
  static const String vagtablesImage="assets/images/vagtables.png";
  static const String enabImage="assets/images/enabbb.png";
+ static const String onBoardingImage="assets/images/shopping.png";
+ static const String signUpImage="assets/images/signUp.png";
+ static const String signInImage="assets/images/login.png";
+ static const String keySvg="assets/images/key.svg";
+ static const String cartSvg="assets/images/cart.svg";
+ static const String logOutSvg="assets/images/logout.svg";
 
 
 
